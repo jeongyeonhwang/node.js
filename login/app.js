98285569517d400120263f0ef3c1e1bcd94ee675
yeon <ouo0222@naver.com>
2023-12-04 19:37:33 +0900

    20231204

--- login/app.js
+++ login/app.js
@@ -1,44 +1,16 @@
-// 로그인 화면 만들기 (hard coding ver.)
+"use strict";
 
+// 모듈
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+// 라우팅
+const home = require("./routes/home");
 
-app.get("/", (req, res) => {
-    res.send(` 
-    <!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        여기는 루트입니다.
-    </body>
-    </html>
-    `);
-});
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get("/login", (req, res) => {
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <input type="text" placeholder="아이디"><br>
-            <input type="text" placeholder="비밀번호"><br>
-            <button>로그인</button>
-        </body>
-        </html>
-    `);
-});
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
-app.listen(PORT, function () {
-    console.log("서버 가동");
-});
+module.exports = app;
